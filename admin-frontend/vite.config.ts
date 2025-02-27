e16d6ccc132ed226e7a3d7cb29b4ed129a757859
Olle Wikström <olle.wikstrom@sogeti.se>
2025-02-27 10:19:53 +0100

    Trying to fix the swa config

--- admin-frontend/vite.config.ts
+++ admin-frontend/vite.config.ts
@@ -1,15 +1,30 @@
+import * as dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as dotenv from "dotenv";
+import { resolve } from "path";
 // https://vite.dev/config/
 
 dotenv.config();
 
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "dist",
+		minify: "terser",
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ["react", "react-dom"],
+					// add other large dependencies here
+				},
+			},
+		},
+		chunkSizeWarningLimit: 1000,
+	},
 	resolve: {
 		alias: {
 			events: "eventemitter3",
+			"@": resolve(__dirname, "src"),
 		},
 	},
 	define: {
